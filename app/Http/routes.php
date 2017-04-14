ddd7a4b7943b0953ab1cfb85136d94159bf8a3b0
郝瑞杰 <490472721@qq.com>
2017-04-14 13:39:04 +0800

    Finish layout and toutes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+get('/', 'StaticPagesController@home')->name('home');
+get('/help', 'StaticPagesController@help')->name('help');
+get('/about', 'StaticPagesController@about')->name('about');
+
+get('signup', 'UsersController@create')->name('signup');
