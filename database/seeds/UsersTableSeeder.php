d474bbfa682dfd8b56a5dbe1d1f41ff410d43a4b
郝瑞杰 <490472721@qq.com>
2017-04-15 13:43:55 +0800

    Add account activations

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,6 +20,7 @@ class UsersTableSeeder extends Seeder
         $user->email = '490472721@qq.com';
         $user->password = bcrypt('password');
         $user->is_admin = true;
+        $user->activated = true;
         $user->save();
     }
 }
